e78676d918206fa913f522079e04b24ce31a03ac
VictorHOgawa <victor.ogawa@gmail.com>
2024-07-09 16:40:05 -0400

    fix app.tsx

--- App.tsx
+++ App.tsx
@@ -26,16 +26,73 @@ import { CategoryProvider } from './src/context/categoryContext';
 import { CategoryTypeProvider } from './src/context/selectedCategoryTypeContext';
 import { initialize } from 'react-native-clarity';
 import { OneSignal } from 'react-native-onesignal';
-import { useEffect } from 'react';
 import { useNavigation } from '@react-navigation/native';
+import { useState, useEffect, useRef } from 'react';
+import * as Device from 'expo-device';
+import * as Notifications from 'expo-notifications';
+import Constants from 'expo-constants';
 
 LogBox.ignoreAllLogs(); //Ignore all log notifications
 console.error = () => {};
 console.warn = () => {};
-// const oneSignalAppId =
-//   Platform.OS === 'ios'
-//     ? '12d3af40-6c6e-414a-97b3-92eeff86371f'
-// : '7703eb6d-2210-47c5-8b41-1500b42f96dc';
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
+
+function handleRegistrationError(errorMessage: string) {
+  alert(errorMessage);
+  throw new Error(errorMessage);
+}
+
+async function registerForPushNotificationsAsync() {
+  if (Platform.OS === 'android') {
+    Notifications.setNotificationChannelAsync('default', {
+      name: 'default',
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: '#FF231F7C',
+    });
+  }
+
+  if (Device.isDevice) {
+    const { status: existingStatus } = await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== 'granted') {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    if (finalStatus !== 'granted') {
+      handleRegistrationError('Permission not granted to get push token for push notification!');
+      return;
+    }
+    const projectId =
+      Constants?.expoConfig?.extra?.eas?.projectId ?? Constants?.easConfig?.projectId;
+    // const projectId = "b08608ca-6f9c-4f5f-b5fd-0649bef51771"
+
+    if (!projectId) {
+      handleRegistrationError('Project ID not found');
+    }
+    try {
+      const pushTokenString = (
+        await Notifications.getExpoPushTokenAsync({
+          projectId,
+        })
+      ).data;
+      console.log(pushTokenString);
+      return pushTokenString;
+    } catch (e: unknown) {
+      handleRegistrationError(`${e}`);
+    }
+  } else {
+    handleRegistrationError('Must use physical device for push notifications');
+  }
+}
+
 export default function App() {
   const [fontsLoaded, fontError] = useFonts({
     Poppins_400Regular,
@@ -51,11 +108,37 @@ export default function App() {
     DancingScript_700Bold,
   });
 
+  const [expoPushToken, setExpoPushToken] = useState('');
+  const [notification, setNotification] = useState<Notifications.Notification | undefined>(
+    undefined
+  );
+  const notificationListener = useRef<Notifications.Subscription>();
+  const responseListener = useRef<Notifications.Subscription>();
+
+  useEffect(() => {
+    registerForPushNotificationsAsync()
+      .then((token) => setExpoPushToken(token ?? ''))
+      .catch((error: any) => setExpoPushToken(`${error}`));
+
+    notificationListener.current = Notifications.addNotificationReceivedListener((notification) => {
+      setNotification(notification);
+    });
+
+    responseListener.current = Notifications.addNotificationResponseReceivedListener((response) => {
+      console.log(response);
+    });
+
+    return () => {
+      notificationListener.current &&
+        Notifications.removeNotificationSubscription(notificationListener.current);
+      responseListener.current &&
+        Notifications.removeNotificationSubscription(responseListener.current);
+    };
+  }, []);
+
   if (!fontsLoaded && !fontError) {
     return null;
   }
-  // OneSignal.initialize(oneSignalAppId);
-  // OneSignal.Notifications.requestPermission(true);
 
   return (
     <AuthProvider>
